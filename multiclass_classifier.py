2ad19ca42e7cf0b671ea1a7812ef0809a43b9c2b
Anna Ilina <ilina.anna@queensu.ca>
2018-06-11 22:52:41 -0400

    plot learning curve and increase size of hidden layers

--- multiclass_classifier.py
+++ multiclass_classifier.py
@@ -19,7 +19,7 @@ import torchvision.transforms as transforms
 
 TRAIN_DATA_DIR = "C:/Users/Anya/Documents/ai4good/face-emotion-analysis/fer2013/Data_Images_Facial_Expressions/Training_Others_Small/"
 VALIDATE_DATA_DIR = "C:/Users/Anya/Documents/ai4good/face-emotion-analysis/fer2013/Data_Images_Facial_Expressions/PrivateTest_Others/"
-BATCH_SIZE = 35
+BATCH_SIZE = 10
 
 TMP_DIR = "C:/Users/Anya/Documents/ai4good/face-emotion-analysis/tmp/"
 
@@ -34,34 +34,31 @@ NUM_CLASSES = 6 #6 for multiclass classifier (we are excluding disgust class)
 #       evaluate using BEST model: currently not working
 #       PROBLEM: sometimes loss goes to NAN if learning rate too high and/or too many epochs. Why? Ask JP. How to fix. Want to do validation from model at epoch with best loss
 
-# transform = transforms.Compose([
-#     transforms.Grayscale(),
-#     transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
-#     transforms.ToTensor()])
-
-transform = transforms.Compose([
-    transforms.Grayscale(),
-    transforms.ToTensor()])
-
 ############################################################
 #
 # LOAD data
 #
 ############################################################
 
-#transform = transforms.ToTensor()
-#train_sampler = sampler.WeightedRandomSampler(weights=[1.0/7]*7, num_samples=400*7, replacement=True)       # want to pick ~400 from each class to train
+# transform = transforms.Compose([
+#     transforms.Grayscale(),
+#     transforms.Resize((IMAGE_HEIGHT, IMAGE_WIDTH)),
+#     transforms.ToTensor()])
+
+# transform = transforms.ToTensor()
+
+transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.ToTensor()])
+
 train_data = torchvision.datasets.ImageFolder(TRAIN_DATA_DIR, transform=transform)
-#train_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, sampler=train_sampler, shuffle=False)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+classes = train_data.classes
+print("Train data classes: {}".format(train_data.classes))
 
-
-#validate_sampler = sampler.WeightedRandomSampler(weights=[1.0/NUM_CLASSES]*NUM_CLASSES, num_samples=100*7, replacement=True)    # want to pick ~100 from each class to validate
 validate_data = torchvision.datasets.ImageFolder(VALIDATE_DATA_DIR, transform=transform)
-#validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=BATCH_SIZE, sampler=validate_sampler, shuffle=False)
 validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=BATCH_SIZE, shuffle=True)
-
-
+print("Validate data classes: {}".format(validate_data.classes))
 
 ############################################################
 #
@@ -88,8 +85,6 @@ validate_loader = torch.utils.data.DataLoader(validate_data, batch_size=BATCH_SI
 # _ = plt.imshow(image0, cmap='gray')
 # plt.show()
 
-print(train_data.classes)
-
 ############################################################
 #
 # DEFINE THE MODEL
@@ -107,7 +102,7 @@ class cnn(nn.Module):
 
         self.block1 = nn.Sequential(
             nn.Conv2d(in_channels = 1,  # 1, because grayscale
-                out_channels = 6,      # model chooses 16 filters
+                out_channels = 32,      # model chooses 16 filters
                 kernel_size = KERNEL_SIZE_CONV,
                 stride = STRIDE,
                 padding = PADDING),
@@ -115,8 +110,8 @@ class cnn(nn.Module):
         )
 
         self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels = 6, # convention: use powers of 2
-                out_channels = 12,
+            nn.Conv2d(in_channels = 32, # convention: use powers of 2
+                out_channels = 64,
                 kernel_size = KERNEL_SIZE_CONV,
                 stride = STRIDE,
                 padding = PADDING),
@@ -124,33 +119,109 @@ class cnn(nn.Module):
         )
 
         self.block3 = nn.Sequential(
-            nn.Conv2d(in_channels = 12,
-                out_channels = 24,
+            nn.Conv2d(in_channels = 64,
+                out_channels = 128,
                 kernel_size = KERNEL_SIZE_CONV,
                 stride = STRIDE,
                 padding = PADDING),
             nn.MaxPool2d(kernel_size = KERNEL_SIZE_POOL)) # now image is 12/2 = 6
 
-        self.block4 = nn.Sequential(
-            nn.Linear(864, NUM_CLASSES)    # in=6*6*24=864, out=7 (7 possible emotions)
+        # self.block4 = nn.Sequential(
+        #     nn.Conv2d(in_channels = 128,
+        #         out_channels = 256,
+        #         kernel_size = KERNEL_SIZE_CONV,
+        #         stride = STRIDE,
+        #         padding = PADDING))
+        #     # No pooling layer this time
+
+        self.block5 = nn.Sequential(
+            nn.Linear(4608, NUM_CLASSES)    # in=6*6*128=4608, out=7 (7 possible emotions)
         )
 
     def forward(self, x):
         out = self.block1(x)
         out = self.block2(out)
         out = self.block3(out)
-        out = out.view(-1, 864)   # flatten for nn.Linear
-        return self.block4(out)
+        # out = self.block4(out)
+        out = out.view(-1, 4608)   # flatten for nn.Linear
+        return self.block5(out)
 
 model = cnn()
 
+############################################################
+#
 # DEFINE OPTIMIZER AND CRITERION
+#
+############################################################
 
-learning_rate = 0.03
+learning_rate = 0.02
 print("learning rate = {}".format(learning_rate))
 
 optimizer = optim.SGD(model.parameters(), lr=learning_rate) # Use Adam() or SGD()
 criterion = nn.CrossEntropyLoss() # lost / cost function
+
+############################################################
+#
+# Define function to compute validation losses
+#
+############################################################
+
+def compute_validation_loss(valModel):
+    #valModel = cnn()
+    #valModel.load_state_dict(torch.load('currentTrainingWeights.pt'))
+    valModel.eval()
+    total = 0
+    correct = 0
+
+    # classifications_matrix = np.zeros((NUM_CLASSES, NUM_CLASSES))
+    # classifications_matrix = classifications_matrix.astype(int)
+
+    val_loss = 0
+    n_iter = 0
+
+    for i, (images, targets) in enumerate(validate_loader):
+        #print(i)
+
+        # Forward pass
+        outputs = valModel(images)
+        loss = criterion(outputs, targets)
+        #total_loss.append(loss)
+        #total_loss += loss
+
+        #_, predicted = torch.max(outputs.data, 1)
+
+        # Statistics
+        # total += targets.size(0)
+        # correct += (predicted == targets).sum()
+        val_loss += loss.data.item()
+
+        # del(outputs)
+        # del(loss)
+
+        # for j in range(len(targets)):
+        #     classifications_matrix[targets[j]][predicted[j]] += 1
+
+        # if i == 0:
+        #     print("First val batch: total={}, correct = {}, predicted={}, target={}".format(total, correct, predicted, targets))
+
+        n_iter += 1
+
+    epoch_loss = val_loss/n_iter
+    # print('Accuracy on the validation set: {}%'.format(100 * correct/total))
+    # print(classifications_matrix)
+    # for i in range(NUM_CLASSES):
+    #     print("Accuracy of {}'s: {}".format(i, classifications_matrix[i][i] /sum(classifications_matrix[i])))
+    return epoch_loss
+
+#..............................
+#..............................
+#..............................
+#..............................
+#..............................
+#..............................
+#..............................
+#..............................
+
 
 ############################################################
 #
@@ -161,8 +232,11 @@ criterion = nn.CrossEntropyLoss() # lost / cost function
 model.train()
 
 t0 = time.time()
-total_loss = []
-num_epochs = 20
+#total_loss = []
+num_epochs = 40
+
+training_losses = []
+validation_losses = []
 
 best_epoch_loss = 10000 # arbitrary large number
 best_epoch_num = 0
@@ -172,6 +246,11 @@ elapsedTime = None
 
 for epoch in range(num_epochs):
 
+    if epoch == 14: # on 15th epoch, lower the learning rate
+        learning_rate = 0.01
+        optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+        print("***Reduced learning rate to {}***".format(learning_rate))
+
     train_loss = 0
     n_iter = 0
 
@@ -180,10 +259,11 @@ for epoch in range(num_epochs):
         # Reset (zero) the gradient buffer
         optimizer.zero_grad()
 
-        # Forward pass
+        # Forward pass (training)
         outputs = model(images)
         loss = criterion(outputs, targets)
-        total_loss.append(loss)
+        #loss_eval = criterion()
+        #total_loss.append(loss)
 
         # Backward pass
         loss.backward()
@@ -192,30 +272,47 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         # Statistics
-        train_loss += loss
+        train_loss += loss.data.item()
         n_iter += 1
 
     epoch_loss = train_loss/n_iter
     #print("loss={}, train_loss={}, n_iter={}, epoch_loss={}".format(loss, train_loss, n_iter, epoch_loss))
-    
+
+    #torch.save(model.state_dict(), 'currentTrainingWeights.pt') 
+    # Compute validation loss
+    #del(outputs)
+    val_loss = compute_validation_loss(model)
+    #val_loss = 1
+    model.train()
+
+
     elapsedTime = time.time() - t0
 
-    print('Epoch: {}/{}, Loss: {:.4f}\tTime Elapsed:  {} minutes {:.4f} seconds'.format(
-        epoch+1, num_epochs, epoch_loss,int(elapsedTime//60), elapsedTime % 60))
+
+
+
+    print('Epoch: {}/{}, Train Loss: {:.4f}\tVal Loss: {:.4f}\tTime Elapsed:  {} minutes {:.4f} seconds'.format(
+        epoch+1, num_epochs, epoch_loss, val_loss, int(elapsedTime//60), elapsedTime % 60))
+    training_losses.append(epoch_loss)
+
+
+    
+
+    validation_losses.append(val_loss)
 
     if epoch == 0:
         best_epoch_loss = epoch_loss
         best_epoch_num = 0
         best_state_dict = model.state_dict()
         print("bestEpoch = {}".format(best_epoch_num + 1))
-        torch.save(model.state_dict(), 'mytraining.pt')     #todo: remove. not best solution
+        torch.save(model.state_dict(), 'bestTrainingWeights.pt')     #todo: remove. not best solution
 
     elif epoch_loss < best_epoch_loss:
         best_epoch_loss = epoch_loss
         best_epoch_num = epoch
         best_state_dict = model.state_dict()
         print("bestEpoch = {}".format(best_epoch_num + 1))
-        torch.save(model.state_dict(), 'mytraining.pt')     #todo: remove. not best solution
+        torch.save(model.state_dict(), 'bestTrainingWeights.pt')     #todo: remove. not best solution
     
 print()
 print("Total Time Elapsed: {} minutes {:.4f} seconds".format(int(elapsedTime//60), elapsedTime % 60))
@@ -235,6 +332,27 @@ pickle.dump(bestModelDict, open(os.path.join(TMP_DIR, "bestModelDict.p"), "wb"))
 
 ############################################################
 #
+# PLOT LEARNING CURVE
+#
+############################################################
+
+def plot_learning_curve(training_losses, validation_losses):
+    plt.figure(figsize=(10,10))
+    plt.title("Learning Curve (Loss vs time)")
+    plt.ylabel("Loss")
+    plt.xlabel("Training Steps")
+    plt.plot(training_losses, label="training")
+    plt.plot(validation_losses, label="validation")
+    plt.legend(loc=1)
+    plt.show()
+
+
+#training_losses = [10,9,8,7,5.5,4,3,2,1]
+#validation_losses = [10,9,8,7,5.5,4.5,3.5,2.5,1.5]
+plot_learning_curve(training_losses, validation_losses)
+
+############################################################
+#
 # EVALUATE THE MODEL
 #
 ############################################################
@@ -242,8 +360,14 @@ pickle.dump(bestModelDict, open(os.path.join(TMP_DIR, "bestModelDict.p"), "wb"))
 # use best model for evaluation
 model = cnn()
 # model.load_state_dict(best_state_dict)
-model.load_state_dict(torch.load('mytraining.pt'))
+model.load_state_dict(torch.load('bestTrainingWeights.pt'))
 
+#classifications = {classID: [0]*NUM_CLASSES for classID in range(NUM_CLASSES)} # adjacency matrix to see which what misclassified labels are being missclassified as
+# classifications_matrix [[0]*NUM_CLASSES]*NUM_CLASSES # adjacency matrix to see which what misclassified labels are being missclassified as
+#                                                 # row i will contain what true labels i were classified as
+#                                                 # columns contain all possible labels (0 .. NUM_CLASSES)
+classifications_matrix = np.zeros((NUM_CLASSES, NUM_CLASSES))
+classifications_matrix = classifications_matrix.astype(int)
 
 model.eval()
 
@@ -261,9 +385,40 @@ for i, (images, targets) in enumerate(validate_loader):
     total += targets.size(0)
     correct += (predicted == targets).sum()
 
+    for j in range(len(targets)):
+        classifications_matrix[targets[j]][predicted[j]] += 1
+
     if i < 3:
         print("total={}, correct = {}, predicted={}, target={}".format(total, correct, predicted, targets))
 
 print(correct)  # tensor(10788)
 print(total)    # 28708
-print('Accuracy on the test set: {}%'.format(100 * correct/total))
+print('Accuracy on the validation set: {}%'.format(100 * correct/total))
+
+print(classifications_matrix)
+for i in range(NUM_CLASSES):
+    print("Accuracy of {}'s: {}".format(i, classifications_matrix[i][i] /sum(classifications_matrix[i])))
+
+print()
+
+########################################################################################################
+########################################################################################################
+########################################################################################################
+
+class_correct = list(0. for i in range(NUM_CLASSES))
+class_total = list(0. for i in range(NUM_CLASSES))
+with torch.no_grad():
+    for data in validate_loader:
+        images, labels = data
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(len(labels)):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(NUM_CLASSES):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
